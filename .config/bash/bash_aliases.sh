ac0e58fddbc4058c5124554d1e679a2776e79713
Michał Grzesiak <mgrzesiak147@gmail.com>
2020-02-26 23:27:21 +0100

    Added .bashrc and extended bash_aliases

--- .config/bash/bash_aliases.sh
+++ .config/bash/bash_aliases.sh
@@ -1,2 +1,9 @@
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 alias q='exit'
 alias r='ranger'
+alias internet="nmtui"
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
+
